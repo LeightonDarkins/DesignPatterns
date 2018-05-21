f6008aac3737fe32fefe395ae15b7f8057ad442f
Leighton Darkins <ljdarkins@gmail.com>
2018-05-21 10:30:31 +0200

    LD - Reorganize Main Method Comments. Examples Of Command Pattern With Lambdas

--- src/com/leightondarkins/CommandPattern/Main.java
+++ src/com/leightondarkins/CommandPattern/Main.java
@@ -5,31 +5,31 @@ public class Main {
         System.out.println("CommandPattern");
 
 //        Basic Command Pattern
-//
-//        RemoteControl remoteControl = new RemoteControl();
 
-//        Light livingRoomLight = new Light();
-//        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
-//        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+        RemoteControl remoteControl = new RemoteControl();
 
-//        remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
-//
-//        System.out.println(remoteControl.toString());
-//
-//        System.out.println("Living Room Light Is On: " + livingRoomLight.isOn());
-//
-//
-//        remoteControl.onButtonPushed(0);
-//
-//        System.out.println("Living Room Light Is On: " + livingRoomLight.isOn());
-//
-//        remoteControl.offButtonPushed(0);
-//
-//        System.out.println("Living Room Light Is On: " + livingRoomLight.isOn());
-//
-//        remoteControl.undoButtonWashPushed();
-//
-//        System.out.println("Living Room Light Is On: " + livingRoomLight.isOn());
+        Light livingRoomLight = new Light();
+        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+
+        remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+
+        System.out.println(remoteControl.toString());
+
+        System.out.println("Living Room Light Is On: " + livingRoomLight.isOn());
+
+
+        remoteControl.onButtonPushed(0);
+
+        System.out.println("Living Room Light Is On: " + livingRoomLight.isOn());
+
+        remoteControl.offButtonPushed(0);
+
+        System.out.println("Living Room Light Is On: " + livingRoomLight.isOn());
+
+        remoteControl.undoButtonWashPushed();
+
+        System.out.println("Living Room Light Is On: " + livingRoomLight.isOn());
 
 //        Commands Using State
 //
@@ -64,43 +64,57 @@ public class Main {
 //
 //        System.out.println(ceilingFan.getSpeed());
 
-        // Macro Command Pattern
+//        Macro Command Pattern
 
-        RemoteControl remoteControl = new RemoteControl();
+//        RemoteControl remoteControl = new RemoteControl();
+//
+//        Light light = new Light();
+//        LightOnCommand lightOn = new LightOnCommand(light);
+//        LightOffCommand lightOff = new LightOffCommand(light);
+//
+//        CeilingFan ceilingFan = new CeilingFan("Living Room");
+//        CeilingFanHighCommand ceilingFanHigh = new CeilingFanHighCommand(ceilingFan);
+//        CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
+//
+//        Command[] partyOn = {lightOn, ceilingFanHigh};
+//        Command[] partyOff = {lightOff, ceilingFanOff};
+//        MacroCommand partyOnMacro = new MacroCommand(partyOn);
+//        MacroCommand partyOffMacro = new MacroCommand(partyOff);
+//
+//        remoteControl.setCommand(0, partyOnMacro, partyOffMacro);
+//
+//        System.out.println(remoteControl.toString());
+//
+//        remoteControl.onButtonPushed(0);
+//
+//        System.out.println(remoteControl.toString());
+//        System.out.println(light.isOn());
+//        System.out.println(ceilingFan.getSpeed());
+//
+//        remoteControl.offButtonPushed(0);
+//
+//        System.out.println(remoteControl.toString());
+//        System.out.println(light.isOn());
+//        System.out.println(ceilingFan.getSpeed());
+//
+//        remoteControl.undoButtonWashPushed();
+//
+//        System.out.println(remoteControl.toString());
+//        System.out.println(light.isOn());
+//        System.out.println(ceilingFan.getSpeed());
 
-        Light light = new Light();
-        LightOnCommand lightOn = new LightOnCommand(light);
-        LightOffCommand lightOff = new LightOffCommand(light);
+//        Command Pattern With Lambdas
+//        only works where the command interface has only one method .execute() in this case...
 
-        CeilingFan ceilingFan = new CeilingFan("Living Room");
-        CeilingFanHighCommand ceilingFanHigh = new CeilingFanHighCommand(ceilingFan);
-        CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
-
-        Command[] partyOn = {lightOn, ceilingFanHigh};
-        Command[] partyOff = {lightOff, ceilingFanOff};
-        MacroCommand partyOnMacro = new MacroCommand(partyOn);
-        MacroCommand partyOffMacro = new MacroCommand(partyOff);
-
-        remoteControl.setCommand(0, partyOnMacro, partyOffMacro);
-
-        System.out.println(remoteControl.toString());
-
-        remoteControl.onButtonPushed(0);
-
-        System.out.println(remoteControl.toString());
-        System.out.println(light.isOn());
-        System.out.println(ceilingFan.getSpeed());
-
-        remoteControl.offButtonPushed(0);
-
-        System.out.println(remoteControl.toString());
-        System.out.println(light.isOn());
-        System.out.println(ceilingFan.getSpeed());
-
-        remoteControl.undoButtonWashPushed();
-
-        System.out.println(remoteControl.toString());
-        System.out.println(light.isOn());
-        System.out.println(ceilingFan.getSpeed());
+//        RemoteControl remoteControl = new RemoteControl();
+//
+//        Light light = new Light();
+//        remoteControl.setCommand(0, light::on, light::off);
+//        remoteControl.setCommand(1, () -> { light.on(); }, () -> { light.off(); });
+//
+//        remoteControl.onButtonPushed(0);
+//
+//        System.out.println(remoteControl.toString());
+//        System.out.println(light.isOn());
     }
 }
